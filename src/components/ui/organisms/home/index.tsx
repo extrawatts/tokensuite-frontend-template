3d7e7dc10605344053f671f3f45fd84ed82f2659
Elvaleryn <eraykaya121@gmail.com>
2021-12-28 20:36:20 +0300

    stack added

--- src/components/ui/organisms/home/index.tsx
+++ src/components/ui/organisms/home/index.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import styles from './home.module.scss';
 import cx from 'classnames';
 import { HomeProps } from 'src/types/components/ui/organisms/home';
+import Stack from '../../molecules/stack';
 
 const Home: React.FC<HomeProps> = ({ className }) => {
   return (
     <div className={cx(styles.home, className)}>
-      Hi there
-      <p>testing</p>
+      <Stack space="s1" splitAfter="splitAfter3">
+        <p>testing</p>
+        <p>testing</p>
+        <div>
+          <p>testing</p>
+          <p>testing</p>
+        </div>
+        <p>testing</p>
+        <p>testing</p>
+      </Stack>
     </div>
   );
 };

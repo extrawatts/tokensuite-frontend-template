79f99f562ff0099cf20d5825851a0bf76f647a4f
Elvaleryn <eraykaya121@gmail.com>
2022-01-28 14:51:39 +0300

    build fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Home from 'src/components/templates/home';
-import { usePostsQuery } from 'src/graphql/generated';
-import useGraphqlClientStore from 'src/store/graphql-client';
+// import { usePostsQuery } from 'src/graphql/generated';
+// import useGraphqlClientStore from 'src/store/graphql-client';
 import useModalsStore from 'src/store/modals';
 const HomePage: NextPage = () => {
-  const { graphqlClient } = useGraphqlClientStore();
-  const { data, isLoading } = usePostsQuery(graphqlClient);
+  //const { graphqlClient } = useGraphqlClientStore();
+  // const { data, isLoading } = usePostsQuery(graphqlClient);
   const { openModal } = useModalsStore();
 
   return (

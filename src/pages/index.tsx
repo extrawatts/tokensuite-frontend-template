831f3faa9dd9fa709585c0654a116d2ab06dd0a4
mertcancet <mertcancet95@gmail.com>
2023-06-13 11:09:20 +0300

    ✨ feat(package.json): add new dependencies to the project ✨ feat(ui): add Accordion component ✨ feat(ui): add Input component ✨ feat(utils): add cn utility function ✨ feat(index.tsx): add Input and Accordion components to the page ✨ feat(globals.css): add new color variables and border style to all elements
    🎨 style(tailwind.config.js): add dark mode support and extend theme with custom colors, container and border radius
    🔧 chore(tsconfig.json): add path mapping for react types to fix import issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,13 @@ import Home from 'src/components/templates/home';
 // import useGraphqlClientStore from 'src/store/graphql-client';
 import useModalsStore from 'src/store/modals';
 import { PROCESS_TYPE, PROVIDERS, TChain } from 'types';
-
+import { Input } from 'src/components/ui/atoms';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from 'src/components/ui/atoms/accordion';
 const LP_STAKING_CONTRACT = '0xC70E50B66F22894000129C5DECfbc645645e2B42';
 const REWARD_TOKEN_CONTRACT = '0x9fe9de8bbc10a1c6ed0c13dae8171aa88193419e';
 const FUND_AMOUNT = '1000';
@@ -37,6 +43,25 @@ const HomePage: NextPage = () => {
       <h1 className="text-3xl font-bold underline text-teal-500 decoration-sky-500">
         Tailwind work
       </h1>
+      <Input />
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other components aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. Its animated by default, but you can disable it if you prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 };
